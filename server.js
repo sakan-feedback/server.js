702cf0313c46fe9d95ce94abfc379848675ec6ed
sakan-feedback <sakanamna6@gmail.com>
2026-04-06 11:37:15 +0400

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -6,21 +12,26 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let feedbacks = []; // مؤقت (لاحقاً نربطه بقاعدة بيانات)
 
-app.post("/feedback", (req, res) => {
-  const data = req.body;
-  feedbacks.push(data);
-  console.log("New Feedback:", data);
-  res.json({ message: "Feedback saved" });
+
+app.post("/feedback", async (req, res) => {
+  const { name, location, comments } = req.body;
+
+  await pool.query(
+    "INSERT INTO feedback (name, location, comments) VALUES ($1, $2, $3)",
+    [name, location, comments]
+  );
+
+  res.json({ message: "Saved" });
 });
 
-app.get("/feedback", (req, res) => {
-  res.json(feedbacks);
 });
-app.get("/", (req, res) => {
-  res.send("SAKAN Feedback System is Running✅");
+
+app.get("/feedback", async (req, res) => {
+  const result = await pool.query("SELECT * FROM feedback ORDER BY id DESC");
+  res.json(result.rows);
 });
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
