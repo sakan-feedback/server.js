66a12abd52a706b166774ad66c128831545f5c32
sakan-feedback <sakanamna6@gmail.com>
2026-04-06 09:46:11 +0400

    Create  server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+let feedbacks = []; // مؤقت (لاحقاً نربطه بقاعدة بيانات)
+
+app.post("/feedback", (req, res) => {
+  const data = req.body;
+  feedbacks.push(data);
+  console.log("New Feedback:", data);
+  res.json({ message: "Feedback saved" });
+});
+
+app.get("/feedback", (req, res) => {
+  res.json(feedbacks);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
